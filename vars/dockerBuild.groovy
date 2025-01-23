3058611f96687e688d54d886f50f97d2157aa9d1
Daniel Swaray <danielswaray09@gmail.com>
2025-01-22 23:13:06 -0800

    added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String dockerHubUsername, String imageName) {
     // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=77c79b7207msh3fcea28d5e8f0dfp1974fbjsn395660057497 -t ${imageName} ."
+    sh "docker build --build-arg REACT_APP_RAPID_API_KEY= '77c79b7207msh3fcea28d5e8f0dfp1974fbjsn395660057497' -t ${imageName}."
      // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
     // Push the Docker image
